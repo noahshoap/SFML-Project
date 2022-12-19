2a54ad57c1ddf88635f5b8a95f9d23f18a6829b8
Noah Shoap <nshoap@vols.utk.edu>
2022-12-19 11:15:30 -0500

    MenuState properly renders text

--- src/MenuState.cpp
+++ src/MenuState.cpp
@@ -6,6 +6,16 @@ MenuState::MenuState(std::shared_ptr<sf::RenderWindow> w) : State(w) {
 
 void MenuState::draw(const float& dt) {
     (void) dt;
+    sf::Font font;
+
+    if (!font.loadFromFile("Arial.ttf")) return;
+
+    sf::Text text;
+    text.setFont(font);
+    text.setString("Hello, world!");
+    text.setCharacterSize(24);
+    text.setFillColor(sf::Color::Green);
+    window->draw(text);
 }
 
 void MenuState::handleInput() {
