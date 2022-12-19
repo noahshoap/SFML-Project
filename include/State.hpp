9b89f4f8837b1340070db2c70431a53a904bb093
Noah Shoap <nshoap@vols.utk.edu>
2022-12-18 20:49:01 -0500

    Starting on a state manager

---
+++ include/State.hpp
@@ -0,0 +1,11 @@
+#ifndef STATE_HPP
+#define STATE_HPP
+
+class State {
+public:
+    virtual void draw(const float&) = 0;
+    virtual void handleInput() = 0;
+    virtual void update(const float&) = 0;
+};
+
+#endif /* STATE_HPP */
---
+++ include/StateManager.hpp
@@ -0,0 +1,19 @@
+#ifndef STATE_MANAGER_HPP
+#define STATE_MANAGER_HPP
+
+#include <stack>
+#include <memory>
+
+#include "State.hpp"
+
+class StateManager {
+public:
+    void addState(std::unique_ptr<State>);
+    std::unique_ptr<State> getState();
+    void removeState();
+    void replaceState(std::unique_ptr<State>);
+private:
+    std::stack<std::unique_ptr<State>> states;
+};
+
+#endif /* STATE_MANAGER_HPP */
