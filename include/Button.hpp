a71de72ae5a87847f6534401ad49f65d71dcf323
Noah Shoap <nshoap@vols.utk.edu>
2022-12-19 19:40:22 -0500

    Beginning TextButton

--- include/Button.hpp
+++ include/Button.hpp
@@ -1,6 +1,8 @@
 #ifndef BUTTON_HPP
 #define BUTTON_HPP
 
+#include <memory>
+#include <string>
 #include <SFML/Graphics.hpp>
 
 class Button : public sf::Drawable {
--- include/TextButton.hpp
+++ include/TextButton.hpp
@@ -2,7 +2,19 @@
 #define TEXT_BUTTON_HPP
 
 class TextButton : public Button {
-
+public:
+    TextButton();
+    TextButton(const sf::Vector2f&);
+    TextButton(const sf::Vector2f&, const sf::Vector2f&);
+    TextButton(const sf::Vector2f&, const sf::Vector2f&, const sf::Color&);
+    TextButton(const sf::Vector2f&, const sf::Vector2f&, const sf::Color&, const std::string&);
+    void setText(const std::string&, const std::string& = "Arial");
+    virtual void clicked() final override;
+    virtual void draw(sf::RenderTarget&, sf::RenderStates) final override;
+protected:
+    std::shared_ptr<sf::Font> font;
+    std::string text_literal;
+    sf::Text text;
 };
 
 #endif /* TEXT_BUTTON_HPP */
