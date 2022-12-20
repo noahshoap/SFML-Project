3a2605a18cd4200984b0837a04d54d54f3907863
Noah Shoap <nshoap@vols.utk.edu>
2022-12-19 22:32:40 -0500

    Playing around with rendering two buttons

--- src/MenuState.cpp
+++ src/MenuState.cpp
@@ -7,9 +7,10 @@ MenuState::MenuState(std::shared_ptr<sf::RenderWindow> w, std::shared_ptr<AssetM
 void MenuState::draw(const float& dt) {
     (void) dt;
 
-    TextButton button(assets, sf::Vector2f(200, 100), sf::Vector2f(300, 250), sf::Color::Green, "Hello!");
-
+    TextButton button(assets, sf::Vector2f(200, 100), sf::Vector2f(300, 250), sf::Color::Green, "Play");
+    TextButton exit_button(assets, sf::Vector2f(200, 100), sf::Vector2f(300, 360), sf::Color::Red, "Exit");
     window->draw(button);
+    window->draw(exit_button);
 }
 
 void MenuState::handleInput() {
